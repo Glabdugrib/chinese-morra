352b338228d897e179eb27a6e68a50318c0276fe
Simone Sada <si.sada93@gmail.com>
2022-06-27 18:02:23 +0200

    Stores match results

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 // Chiedi numero di match vinti per concludere il gioco
 
 int matchNum = 0;
+int maxMatch = 5; // limite massimo partite
 
 do
 {
@@ -13,56 +14,38 @@ do
     {
         Console.WriteLine("The number of matches must be greater or equal than 1. Try again");
     }
-    else if(matchNum > 10)
+    else if(matchNum > maxMatch)
     {
-        Console.WriteLine("The number of matches must be lower or equal than 10. Try again");
+        Console.WriteLine($"The number of matches must be lower or equal than {maxMatch}. Try again");
     }
 }
-while (matchNum < 1 || matchNum > 10);
+while (matchNum < 1 || matchNum > maxMatch);
 
 
 // LOGICA DEL GIOCO
 int userScore = 0;
 int cpuScore = 0;
+string[] matchSaves = new string[100];
+matchSaves[0] = "Match\t\tYou\t\tCPU\t\tWinner";
+int matchCounter = 1; // contatore partita
 
 do
 {
-    // User choice
+    // Scelta utente
     string input;
-    int userPick = 0;
     bool isInputCorrect = false;
+
+    Console.Write("\n***\n");
 
     do
     {
         Console.Write("\nChoose 'stone', 'paper' or 'scissors': ");
 
-        input = Console.ReadLine();
+        input = Console.ReadLine().ToLower();
 
-        if (input == "stone" || input == "paper" || input == "scissors" || input == "Stone" || input == "Paper" || input == "Scissors")
+        if (input == "stone" || input == "paper" || input == "scissors")
         {
             isInputCorrect = true;
-
-            switch (input)
-            {
-                case "stone":
-                    userPick = 1;
-                    break;
-                case "Stone":
-                    userPick = 1;
-                    break;
-                case "paper":
-                    userPick = 2;
-                    break;
-                case "Paper":
-                    userPick = 2;
-                    break;
-                case "scissors":
-                    userPick = 3;
-                    break;
-                case "Scissors":
-                    userPick = 3;
-                    break;
-            }
         }
         else
         {
@@ -72,68 +55,82 @@ do
     while (!isInputCorrect);
 
 
-    // Cpu choice
+    // Scelta cpu
     Random rnd = new Random();
     int cpuPick = rnd.Next(1, 4);
+    string cpuInput = "stone";
 
-
-    // Print choices
-    Console.Write($"\nUser: ");
-    switch (userPick)
-    {
-        case 1:
-            Console.Write("stone");
-            break;
-        case 2:
-            Console.Write("paper");
-            break;
-        case 3:
-            Console.Write("scissors");
-            break;
-    }
-
-    Console.Write($"\nCPU: ");
     switch (cpuPick)
     {
         case 1:
-            Console.Write("stone");
+            cpuInput = "stone";
             break;
         case 2:
-            Console.Write("paper");
+            cpuInput = "paper";
             break;
         case 3:
-            Console.Write("scissors");
+            cpuInput = "scissors";
             break;
     }
+
+
+    // Stampa scelte
+    Console.Write($"\nUser: {input}");
+
+    Console.Write($"\nCPU: {cpuInput}");
 
     Console.WriteLine();
 
 
-    // Print result
-    if (userPick == cpuPick)
+    // Stampa risultato
+    string result = "draw";
+
+    if (input == cpuInput)
     {
         Console.WriteLine("\nDraw");
     }
-    else if ((userPick == 1 && cpuPick == 3) || (userPick == 2 && cpuPick == 1) || (userPick == 3 && cpuPick == 2))
+    else if ((input == "stone" && cpuInput == "scissors") || (input == "paper" && cpuInput == "stone") || (input == "scissors" && cpuInput == "paper"))
     {
+        result = "user";
         Console.WriteLine("\nYou win the match!");
         userScore++;
     }
     else
     {
+        result = "cpu";
         Console.WriteLine("\nCPU wins the match!");
         cpuScore++;
     }
+
+    // Salva scelte
+    matchSaves[matchCounter] = $"{matchCounter}\t\t{input}\t{(input != "scissors" ? "\t" : "")}{cpuInput}\t{(cpuInput != "scissors" ? "\t" : "")}{result}";
+    matchCounter++;
 
     Console.WriteLine($"User: {userScore} vs CPU: {cpuScore}");
 }
 while (userScore < matchNum && cpuScore < matchNum);
 
-if(userScore == matchNum)
+
+// stampa vincitore del gioco
+Console.Write("\n***\n***\n");
+
+if (userScore == matchNum)
 {
     Console.WriteLine("\nYOU WIN THE GAME!!");
 }
 else
 {
     Console.WriteLine("\nCPU WINS THE GAME!!");
+}
+
+
+// stampa scelte di tutte le partite
+Console.WriteLine();
+
+for (int i = 0; i < matchSaves.Length; i++)
+{
+    if(matchSaves[i] != null)
+    {
+        Console.WriteLine(matchSaves[i]);
+    }
 }
